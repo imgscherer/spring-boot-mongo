c2cdc627d40b51398d467bd990e616942538a592
Gabriel Scherer <gabrielscherer2709@gmail.com>
2022-05-23 20:13:59 -0300

    Author DTO created

--- src/main/java/com/example/springbootwithmongo/config/Instantiation.java
+++ src/main/java/com/example/springbootwithmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.example.springbootwithmongo.domain.Post;
 import com.example.springbootwithmongo.domain.User;
+import com.example.springbootwithmongo.dto.AuthorDTO;
 import com.example.springbootwithmongo.repository.PostRepository;
 import com.example.springbootwithmongo.repository.UserRepository;
 
@@ -34,11 +35,12 @@ public class Instantiation implements CommandLineRunner{
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com");
 		User u3 = new User(null, "Bob Grey", "bob@gmail.com");
-		
-		Post p1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", u1);
-		Post p2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", u1);
-		
+	
 		userRepository.saveAll(Arrays.asList(u1, u2, u3));	
+		
+		Post p1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(u1));
+		Post p2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(u1));
+	
 		postRepository.saveAll(Arrays.asList(p1, p2));	
 	}
 
