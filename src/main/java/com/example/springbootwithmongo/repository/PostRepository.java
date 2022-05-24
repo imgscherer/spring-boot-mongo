3422cea073f5909a07a1582fd39c763de5f3c83a
Gabriel Scherer <gabrielscherer2709@gmail.com>
2022-05-23 21:17:56 -0300

    Search with @Query

--- src/main/java/com/example/springbootwithmongo/repository/PostRepository.java
+++ src/main/java/com/example/springbootwithmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.example.springbootwithmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.springbootwithmongo.domain.Post;
@@ -10,5 +11,10 @@ import com.example.springbootwithmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 	
+	//Query do MongoDB
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> findByTitle(String text);
+
+	//Query do Spring Boot
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
